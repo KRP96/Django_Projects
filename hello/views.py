e98216f861576f37317915cb17af1b23845e04a9
KRP96 <kavindapeiris1996@gmail.com>
2021-07-13 11:35:20 +0530

    added return render line anfd create html page

--- hello/views.py
+++ hello/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")        #use HttpResponse keyword
+    return render(request, "hello/index.html")       
 
 
 def brian(request):
-    return HttpResponse("Hello, Brian!")
+    return HttpResponse("Hello, Brian!")  #use HttpResponse keyword
 
 def david(request):
     return HttpResponse("Hello, David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name}!")
+    return HttpResponse(f"Hello, {name.capitalize()}!")
