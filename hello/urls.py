1b71489e26c014ca3f1042c486b56ec9bc4ed855
KRP96 <kavindapeiris1996@gmail.com>
2021-07-13 11:08:26 +0530

    added few linews

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from. import views         #import views.py
 
 urlpatterns = [
-    path("", views.index, name="index")                         #first arguments is "" empty and second argument is function name index in views.py
+    path("", views.index, name="index"),    #first arguments is "" empty and second argument is function name index in views.py  URL=http://127.0.0.1:8000/hello/
+    path("<str:name>", views.greet, name="greet"),  #we can use any name with that. ex : http://127.0.0.1:8000/hello/Kavinda   output==> Hello, Kavinda!
+    path("brian", views.brian, name="brian"), #Added new line URL=http://127.0.0.1:8000/hello/brian
+    path("david", views.david, name= "david") ,   #URL=http://127.0.0.1:8000/hello/david                
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")        #use HttpResponse keyword
+    return HttpResponse("Hello!")        #use HttpResponse keyword
+
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name}!")
